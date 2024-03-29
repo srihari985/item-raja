bbb2c17a3673f2a6a9f4cdfaf4b4de1dc9821af2
srihari985 <123962312+srihari985@users.noreply.github.com>
2024-03-29 20:12:22 +0530

    Create MyServlet.java

---
+++ mywebapp/src/main/java/com/mycompany/MyServlet.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MyServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
